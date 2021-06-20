b65548c22f427b698d7c4441fec64264fc6fb3b3
Emanuele <bosonie@tcd.ie>
2021-06-20 23:56:35 +0200

    Remove roles and update the aiida role using a new branch
    The new branch fixes the problem of database set up
    introduced by psycopg2-binary release and also adds Tb2J and
    banddownfolder

--- roles/marvel-nccr.aiida/files/ipython_config.py
+++
@@ -1,6 +0,0 @@
-# https://ipython.readthedocs.io/en/stable/config/intro.html#python-configuration-files
-c = get_config()
-c.InteractiveShellApp.extensions = [
-  # this allows the use of the %aiida magic
-  'aiida.tools.ipython.ipython_magics'
-]
